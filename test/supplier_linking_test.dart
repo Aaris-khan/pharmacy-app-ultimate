4175276d2be578e8caa42c4ce1dfda02feb4a14a
warishakhan3548-hash <warishakhan3548@gmail.com>
2026-09-20 01:31:15 +0530

    Fix large supplier fixture map key

--- test/supplier_linking_test.dart
+++ test/supplier_linking_test.dart
@@ -337,7 +337,7 @@ void main() {
       MemoryInventoryStorage(
         InventorySnapshot(
           records: {for (final medicine in medicines) medicine.id: medicine},
-          suppliers: <String, Supplier>{supplier.id: supplier},
+          suppliers: <String, Supplier>{'supplier_large': supplier},
         ),
       ),
       clock: () => today,
